53ea092c2f2ba96b90621bde8f27e6ce11cf7e13
Mairon Vilela <maironvilela@hotmail.com>
2022-03-13 20:11:25 -0300

    docs: storybook Logo component

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Logo } from '.';
+
+export default {
+  title: 'Logo',
+  component: Logo
+} as ComponentMeta<typeof Logo>;
+
+export const Default: ComponentStory<typeof Logo> = () => <Logo />;
--- src/components/Logo/styles.tsx
+++ src/components/Logo/styles.tsx
@@ -4,16 +4,16 @@ export const Container = styled.div`
   position: relative;
   > svg {
     color: #034afd;
-    top: 20px;
-    left: -10px;
     position: relative;
+    width: 3.5rem;
+    height: 3.5rem;
 
     & + svg {
       position: relative;
       color: #00e095;
       fill-opacity: 0.85;
-      top: -11px;
-      left: -5px;
+      top: 0.6rem;
+      left: -3rem;
     }
   }
 `;
