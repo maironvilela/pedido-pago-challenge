35a5572a3d622887c12b7ac054396ab7912661c2
Mairon Vilela <maironvilela@hotmail.com>
2022-03-17 19:15:29 -0300

    test: Badge Componente ensure render the Badges with inactive description end your main styles

--- src/components/Badge/test.tsx
+++ src/components/Badge/test.tsx
@@ -3,12 +3,12 @@ import { renderWithTheme } from '../../utils/tests/helpers';
 import { Badge } from '.';
 
 describe('<Badge />', () => {
-  it('should be able to render the Badges with description Active', () => {
+  it('should be able to render the Badges with Active description ', () => {
     renderWithTheme(<Badge description="Ativo" type="active" />);
     expect(screen.getByText(/Ativo/i)).toBeInTheDocument();
   });
 
-  it('should be able to render the Badges Active with main styles', () => {
+  it('should be able to render the Badges with active description end your main styles', () => {
     renderWithTheme(<Badge description="Ativo" type="active" />);
     expect(screen.getByText(/Ativo/i)).toHaveStyle({
       background: '#B5F1DD',
@@ -18,8 +18,18 @@ describe('<Badge />', () => {
     });
   });
 
-  it('should be able to render the Badges with description Active', () => {
+  it('should be able to render the Badges with description Inactive', () => {
     renderWithTheme(<Badge description="Inativo" type="inactive" />);
     expect(screen.getByText(/Inativo/i)).toBeInTheDocument();
+  });
+
+  it('should be able to render the Badges with inactive description end your main styles', () => {
+    renderWithTheme(<Badge description="Inativo" type="inactive" />);
+    expect(screen.getByText(/inativo/i)).toHaveStyle({
+      background: '#EAEFED',
+      color: '#34423d',
+      'font-weight': 500,
+      'font-size': '1.4rem'
+    });
   });
 });
