a1875a1cec311c63bbd77be7f8c50f08d6dec8eb
Mairon Vilela <maironvilela@hotmail.com>
2022-03-17 09:58:29 -0300

    chore: config storybook themeProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '@emotion/react';
+import { ThemeProvider } from 'emotion-theming';
 import theme from '../src/styles/theme';
 import { GlobalStyle } from '../src/styles/global';
 
