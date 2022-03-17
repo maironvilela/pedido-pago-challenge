484b5642f84ba26d2ab293e6607c2e8874dd5110
Mairon Vilela <maironvilela@hotmail.com>
2022-03-17 18:44:30 -0300

    test: Badge Componente ensure render the Badges with description Inactive

--- src/components/Badge/test.tsx
+++ src/components/Badge/test.tsx
@@ -3,8 +3,13 @@ import { renderWithTheme } from '../../utils/tests/helpers';
 import { Badge } from '.';
 
 describe('<Badge />', () => {
-  it('should be able to render description Active', () => {
-    renderWithTheme(<Badge description="Active" type="active" />);
-    expect(screen.getByText(/Active/i)).toBeInTheDocument();
+  it('should be able to render the Badges with description Active', () => {
+    renderWithTheme(<Badge description="Ativo" type="active" />);
+    expect(screen.getByText(/Ativo/i)).toBeInTheDocument();
+  });
+
+  it('should be able to render the Badges with description Active', () => {
+    renderWithTheme(<Badge description="Inativo" type="inactive" />);
+    expect(screen.getByText(/Inativo/i)).toBeInTheDocument();
   });
 });
