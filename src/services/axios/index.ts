eb9b92744b23b1bc44924873de1d6256c04f1d5d
Mairon Vilela <maironvilela@hotmail.com>
2022-04-11 20:13:27 -0300

    fix: config Environment Variables axios service

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL ?? 'http://localhost:3000/api'
+  baseURL:
+    process.env.API_URL ??
+    process.env.NEXT_PUBLIC_API_URL ??
+    'http://localhost:3000/api'
 });
